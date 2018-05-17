988c06a9bb3eb468fbf59fe1163f6445ac0064fa
Ethan J. Brown <Iristyle@github>
2018-05-17 15:15:39 -0700

    (MODULES-7120) Remove V2::append_v1trigger
     - Dead code, no callers

--- lib/puppet_x/puppetlabs/scheduled_task/trigger.rb
+++ lib/puppet_x/puppetlabs/scheduled_task/trigger.rb
@@ -772,51 +772,6 @@ module Trigger
       manifest_hash
     end
 
-    def self.append_v1trigger(definition, v1trigger)
-      v1trigger = Trigger::V1.canonicalize_and_validate(v1trigger)
-
-      trigger_type = type_from_v1type(v1trigger['trigger_type'])
-      iTrigger = definition.Triggers.Create(trigger_type)
-      trigger_settings = v1trigger['type']
-
-      case trigger_type
-        when Type::TASK_TRIGGER_DAILY
-          # https://msdn.microsoft.com/en-us/library/windows/desktop/aa446858(v=vs.85).aspx
-          iTrigger.DaysInterval = trigger_settings['days_interval']
-
-        when Type::TASK_TRIGGER_WEEKLY
-          # https://msdn.microsoft.com/en-us/library/windows/desktop/aa384019(v=vs.85).aspx
-          iTrigger.DaysOfWeek = trigger_settings['days_of_week']
-          iTrigger.WeeksInterval = trigger_settings['weeks_interval']
-
-        when Type::TASK_TRIGGER_MONTHLY
-          # https://msdn.microsoft.com/en-us/library/windows/desktop/aa382062(v=vs.85).aspx
-          iTrigger.DaysOfMonth = trigger_settings['days']
-          iTrigger.Monthsofyear = trigger_settings['months']
-
-        when Type::TASK_TRIGGER_MONTHLYDOW
-          # https://msdn.microsoft.com/en-us/library/windows/desktop/aa382055(v=vs.85).aspx
-          iTrigger.DaysOfWeek = trigger_settings['days_of_week']
-          iTrigger.Monthsofyear = trigger_settings['months']
-          # HACK: convert V1 week value to names, then back to V2 bitmask
-          week_name = Trigger::V1::Occurrence.constant_to_name(trigger_settings['weeks'])
-          iTrigger.Weeksofmonth = WeeksOfMonth.names_to_bitmask(week_name)
-      end
-
-      # Values for all Trigger Types
-      iTrigger.Repetition.Interval = "PT#{v1trigger['minutes_interval']}M" unless v1trigger['minutes_interval'].nil? || v1trigger['minutes_interval'].zero?
-      iTrigger.Repetition.Duration = "PT#{v1trigger['minutes_duration']}M" unless v1trigger['minutes_duration'].nil? || v1trigger['minutes_duration'].zero?
-      iTrigger.StartBoundary = Trigger.date_components_to_local_iso8601_datetime(
-        v1trigger['start_year'],
-        v1trigger['start_month'],
-        v1trigger['start_day'],
-        v1trigger['start_hour'],
-        v1trigger['start_minute']
-      )
-
-      v1trigger
-    end
-
     def self.append_trigger(definition, manifest_hash)
       manifest_hash = Trigger::V1.canonicalize_and_validate_manifest(manifest_hash)
       # create appropriate ITrigger based on 'schedule'
